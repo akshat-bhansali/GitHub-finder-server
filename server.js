04ec9f697ed8075772b46624eca6f5a5e656b545
akshatbhansali <akshatb2198@gmail.com>
2024-02-02 18:22:34 +0530

    switched to local

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const io = require("socket.io")(3001, {
-  cors: { origin: "https://git-hub-finder-alpha.vercel.app", methods: ["GET", "POST"] },
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
 });
 
 io.on("connection",(socket)=>{
